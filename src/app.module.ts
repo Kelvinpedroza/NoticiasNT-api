b51e1b03a8a9c2f294704c0063172568d848d701
kelvin <kelvinpedroza@gmail.com>
2024-08-19 22:00:47 -0300

    add dataBase config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { CommentsModule } from './comments/comments.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), CommentsModule, UsersModule, AuthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), CommentsModule, UsersModule, AuthModule, DbModule],
   controllers: [AppController],
   providers: [AppService],
 })
