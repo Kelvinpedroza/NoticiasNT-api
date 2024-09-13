ca65f0113fbedd95ef7cedb97f239f53682ebcf3
kelvin <kelvinpedroza@gmail.com>
2024-09-12 23:24:28 -0300

    changer

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -1,21 +1,45 @@
+// import { Module } from '@nestjs/common';
+// import { ConfigService } from '@nestjs/config';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+
+// @Module({
+//     imports: [TypeOrmModule.forRootAsync({
+//         useFactory: async (configService: ConfigService) => ({
+//             type: 'postgres',
+//             host: configService.get<string>('DB_HOST'),
+//             port: +configService.get<number>('DB_PORT'),
+//             username: configService.get<string>('DB_USERNAME'),
+//             password: configService.get<string>('DB_PASSWORD'),
+//             database: configService.get<string>('DB_NAME'),
+//             entities: [__dirname + '/entities/**'],
+//             migrations: [__dirname + '/migrations/*.ts'],
+//             synchronize: false
+//         }),
+//         inject: [ConfigService]
+//     })]
+// })
+// export class DbModule { }
+
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-    imports: [TypeOrmModule.forRootAsync({
-        useFactory: async (configService: ConfigService) => ({
-            type: 'postgres',
-            host: configService.get<string>('DB_HOST'),
-            port: +configService.get<number>('DB_PORT'),
-            username: configService.get<string>('DB_USERNAME'),
-            password: configService.get<string>('DB_PASSWORD'),
-            database: configService.get<string>('DB_NAME'),
-            entities: [__dirname + '/entities/**'],
-            migrations: [__dirname + '/migrations/*.ts'],
-            synchronize: false
+    imports: [
+        TypeOrmModule.forRootAsync({
+            useFactory: async (configService: ConfigService) => ({
+                type: 'postgres',
+                url: configService.get<string>('DATABASE_URL'), // Usando a URL completa
+                entities: [__dirname + '/entities/**'],
+                migrations: [__dirname + '/migrations/*.ts'],
+                synchronize: false,
+                ssl: {
+                    rejectUnauthorized: false, // Necessário para o Neon
+                },
+            }),
+            inject: [ConfigService],
         }),
-        inject: [ConfigService]
-    })]
+    ],
 })
-export class DbModule { }
+export class DbModule {}
+
