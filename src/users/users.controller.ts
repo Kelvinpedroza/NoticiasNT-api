fbf84e503df8e747a8863912052fb54c5e6fe3dd
kelvin <kelvinpedroza@gmail.com>
2024-09-14 21:06:00 -0300

    fixx

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,12 +3,12 @@ import { UsersService } from './users.service';
 import { UserDto } from './user.dto';
 import { AuthGuard } from '../auth/auth.guard';
 
-@UseGuards(AuthGuard)
+
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) { }
 
-    @Post()
+    @Post('cadastro')
     create(@Body() user: UserDto) {
         this.usersService.create(user)
     }
