3b369898525075f0ac925e27c01f040035c79dd9
kelvin <kelvinpedroza@gmail.com>
2024-09-17 20:31:39 -0300

    heroku Deploy

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+const port = process.env.PORT  || 3000
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({transform: true }))
+  // app.enableCors({
+  //   origin: true,
+  //   methods:"GET, HEAD,PUT,PATCH,DELETE,OPTIONS",
+  //   credentials: true,
+  // })
   app.enableCors({
-    origin: true,
-    methods:"GET, HEAD,PUT,PATCH,DELETE,OPTIONS",
-    credentials: true,
-  })
-  await app.listen(3000);
+    origin: '*', // Substitua pelo seu domínio ou use '*' para permitir todos os domínios
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE, OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+  await app.listen(port);
 }
 bootstrap();
