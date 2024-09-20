637e68d38473e6b7afdfd6b0d9da09e749fc3c8a
kelvin <kelvinpedroza@gmail.com>
2024-09-19 22:26:53 -0300

    changer cors roles

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({transform: true }))
   app.enableCors({
-    origin: true,
+    origin: '*',
     methods:"GET, HEAD,PUT,PATCH,DELETE,OPTIONS",
     credentials: true,
   })
