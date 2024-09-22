63a365bd6a19667729d66622559564240328daeb
kelvin <kelvinpedroza@gmail.com>
2024-09-22 13:34:32 -0300

    fixx conflit

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,20 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({transform: true }))
   app.enableCors({
+<<<<<<< HEAD
     origin: '*',
     methods:"GET, HEAD,PUT,PATCH,DELETE,OPTIONS",
     credentials: true,
   })
   app.setGlobalPrefix('api/');
   await app.listen(3000);
+=======
+     origin: true,
+     methods:"GET, HEAD,PUT,PATCH,DELETE,OPTIONS",
+     credentials: true,
+   })
+    app.setGlobalPrefix('api/');
+>>>>>>> 2525145 (add /)
 }
 
 bootstrap();
