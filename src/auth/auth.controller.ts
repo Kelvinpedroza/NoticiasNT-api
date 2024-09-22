0d04021ee7ca19baba7fa699cef9687f56584e65
kelvin <kelvinpedroza@gmail.com>
2024-09-22 13:19:48 -0300

    fix login email

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,7 +8,7 @@ export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
     @Post('login')
-    async signIn(@Body('username') username: string ,@Body('password') password: string):Promise<AuthResponseDto> {
-        return await this.authService.signIn(username,password)
+    async signIn(@Body('email') email: string ,@Body('password') password: string):Promise<AuthResponseDto> {
+        return await this.authService.signIn(email,password)
     }
 }
