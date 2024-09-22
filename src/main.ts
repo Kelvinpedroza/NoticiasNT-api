a4ce648cd3eeed8c5d6c7fa3b8d2dadeaab3561c
kelvin <kelvinpedroza@gmail.com>
2024-09-22 13:10:29 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,6 @@ const port = process.env.PORT  || 3000
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({transform: true }))
-  // app.enableCors({
-  //   origin: true,
-  //   methods:"GET, HEAD,PUT,PATCH,DELETE,OPTIONS",
-  //   credentials: true,
-  // })
   app.enableCors({
     origin: '*',
     methods:"GET, HEAD,PUT,PATCH,DELETE,OPTIONS",
@@ -20,4 +15,5 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   await app.listen(3000);
 }
+
 bootstrap();
